8a52db0b063a9b94602fbd7e25c42badd6cc09cc
Shreya <ss.shankar505@gmail.com>
2021-03-19 14:02:57 -0700

    Skeleton for Store class

---
+++ mltrace/db/store.py
@@ -0,0 +1,35 @@
+class Store(object):
+    """Helper methods to interact with the db."""
+
+    def __init__(self):
+        pass
+
+    def create_component(self):
+        pass
+
+    def create_empty_component_run(self):
+        pass
+
+    def create_io_pointer(self):
+        pass
+
+    def delete_component(self):
+        pass
+
+    def delete_component_run(self):
+        pass
+
+    def delete_io_pointer(self):
+        pass
+
+    def create_output_ids(self, num_outputs):
+        pass
+
+    def commit_component_run(self):
+        pass
+
+    def trace(self, output_id):
+        pass
+
+    def trace_batch(self, output_ids):
+        pass
