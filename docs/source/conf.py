6165f5ccc2e1098169535625e554a7d6cad42a60
Shreya <ss.shankar505@gmail.com>
2021-04-12 17:27:13 -0700

    index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Shreya Shankar'
 
 # The full version, including alpha/beta/rc tags
 release = '0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
