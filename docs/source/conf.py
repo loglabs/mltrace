f04c57ee665e5428e2742932dabf416d96998b63
Shreya <ss.shankar505@gmail.com>
2021-04-12 17:26:28 -0700

    Master doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ author = 'Shreya Shankar'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
+master_doc = 'index'
+
 
 # -- General configuration ---------------------------------------------------
 
