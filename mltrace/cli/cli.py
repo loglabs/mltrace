9b8f75ce85c1c1540b5c9df4e2f2ac3a2a9e6a73
ariG23498 <aritra.born2fly@gmail.com>
2021-05-13 10:21:27 +0530

    history and trace added

--- mltrace/cli/cli.py
+++ mltrace/cli/cli.py
@@ -3,11 +3,12 @@
 
 import click
 from mltrace.client import (
-    # _db_uri,
     set_address,
     get_recent_run_ids,
     get_component_run_information,
     get_component_information,
+    get_history,
+    web_trace,
 )
 import textwrap
 
@@ -16,10 +17,22 @@ import textwrap
 
 
 def set_server(url: str):
+    """
+    Set the database server connection.
+
+    Args:
+        url: The db url.
+    """
     set_address(url)
 
 
 def show_info_card(run_id: int):
+    """
+    Prints the info cards corresponding to run ids.
+
+    Args:
+        run_id: The component run id.
+    """
     cr_info = get_component_run_information(run_id)
     c_info = get_component_information(cr_info.component_name)
 
@@ -38,25 +51,126 @@ def show_info_card(run_id: int):
     inputs = cr_info.inputs
     for idx, inp in enumerate(inputs):
         if idx == len(inputs) - 1:
-            click.echo(f"│  └{inp['name']}")
+            click.echo(f"│  └─{inp['name']}")
         else:
             click.echo(f"│  ├─{inp['name']}")
     click.echo("├─Outputs:")
     outputs = cr_info.outputs
     for idx, out in enumerate(outputs):
         if idx == len(outputs) - 1:
-            click.echo(f"│  └{out['name']}")
+            click.echo(f"│  └─{out['name']}")
         else:
             click.echo(f"│  ├─{out['name']}")
     code = textwrap.indent(cr_info.code_snapshot, "│  ")
     click.echo(f"├─Code Snapshot:\n{code.rstrip()}")
-    dependencies = (
-        " ".join(cr_info.dependencies)
-        if cr_info.dependencies
-        else "None"
-    )
-    click.echo(f"└Dependencies: {dependencies}")
+    dependencies = " ".join(cr_info.dependencies) if cr_info.dependencies else "None"
+    click.echo(f"└─Dependencies: {dependencies}")
     click.echo()
+
+
+def show_history(history):
+    """
+    Prints the history as a info card.
+
+    Args:
+        history: History object.
+    """
+    for hist in history:
+        click.echo(f"{hist.component_name}--{hist.id}")
+        click.echo(f"├─Started: {hist.start_timestamp}")
+        click.echo(f"├─Git: {hist.git_hash}")
+        elapsed_time = hist.end_timestamp - hist.start_timestamp
+        min, sec = divmod(elapsed_time.total_seconds(), 60)
+        min = min + 1e-1 * sec
+        click.echo(f"├─Duration: {min:0.3f} mins")
+        click.echo("├─Inputs:")
+        inputs = hist.inputs
+        for idx, inp in enumerate(inputs):
+            if idx == len(inputs) - 1:
+                click.echo(f"│  └─{inp['name']}")
+            else:
+                click.echo(f"│  ├─{inp['name']}")
+        click.echo("├─Outputs:")
+        outputs = hist.outputs
+        for idx, out in enumerate(outputs):
+            if idx == len(outputs) - 1:
+                click.echo(f"│  └─{out['name']}")
+            else:
+                click.echo(f"│  ├─{out['name']}")
+        code = textwrap.indent(hist.code_snapshot, "│  ")
+        click.echo(f"├─Code Snapshot:\n{code.rstrip()}")
+        dependencies = " ".join(hist.dependencies) if hist.dependencies else "None"
+        click.echo(f"└─Dependencies: {dependencies}")
+        click.echo()
+
+
+def show_res(res, indent, count, pos, need_stick):
+    """
+    A recursive method that prints the trace of an output id.
+
+    The response of `web_trace` is a list of nodes. The nodes
+    can either be a list of children nodes or a dictionary.
+
+    We will recursively iterate the res, and print the `label`
+    field of each node.
+
+    Args:
+        res: the node, can either be a list or dict.
+        indent: required indentation. (Used for prinitng)
+        count: how many children. (Used for prinitng)
+        pos: which child. (Used for prinitng)
+        need_stick: how many "|" are needed. (Used for prinitng)
+    """
+    if isinstance(res, dict):
+        # dictionary is a node
+        if count == pos:
+            # Last child
+            label = f"└─{res['label']}"
+
+            pre = "  " if indent > 0 else ""
+            sticks = "│ " * (need_stick)
+            temp_indent = (indent - 1) - (need_stick)
+            post = "  " * (temp_indent)
+
+            label = pre + sticks + post + label
+            click.echo(label)
+            if "childNodes" in res.keys():
+                show_res(
+                    res=res["childNodes"],
+                    indent=indent + 1,
+                    count=count,
+                    pos=pos,
+                    need_stick=need_stick,
+                )
+        else:
+            label = f"├─{res['label']}"
+
+            pre = "  " if indent > 0 else ""
+            sticks = "│ " * (need_stick)
+            temp_indent = (indent - 1) - (need_stick)
+            post = "  " * (temp_indent)
+
+            label = pre + sticks + post + label
+            click.echo(label)
+            if "childNodes" in res.keys():
+                show_res(
+                    res=res["childNodes"],
+                    indent=indent + 1,
+                    count=count,
+                    pos=pos,
+                    need_stick=need_stick + 1,
+                )
+
+    if isinstance(res, list):
+        # list of children
+        for index, component in enumerate(res):
+            show_res(
+                res=component,
+                indent=indent,
+                count=len(res) - 1,
+                pos=index,
+                need_stick=need_stick,
+            )
 
 
 # ------------------------- CLI ------------------------ #
@@ -71,8 +185,7 @@ def mltrace():
 @click.option("--url", default="localhost", help="URL of the database.")
 @click.option("--count", default=5, help="Count of recent objects.")
 def recent(
-    url: str,
-    count: int,
+    url: str, count: int,
 ):
     """
     CLI for recent objects.
@@ -83,3 +196,36 @@ def recent(
     component_run_ids = get_recent_run_ids()
     for id in component_run_ids[:count]:
         show_info_card(id)
+
+
+@mltrace.command("history")
+@click.option("--url", default="localhost", help="URL of the database.")
+@click.option("--count", default=5, help="Count of recent objects.")
+@click.argument("component_name")
+def history(
+    url: str, component_name: str, count: int,
+):
+    """
+    CLI for history of ComponentName.
+    """
+    # Set Server
+    set_server(url)
+    history = (
+        get_history(component_name, count) if count else get_history(component_name)
+    )
+    show_history(history)
+
+
+@mltrace.command("trace")
+@click.option("--url", default="localhost", help="URL of the database.")
+@click.argument("output_id")
+def trace(
+    url: str, output_id: str,
+):
+    """
+    CLI for trace.
+    """
+    # Set db
+    set_address(url)
+    res = web_trace(output_id)
+    show_res(res=res, indent=0, count=0, pos=0, need_stick=0)
