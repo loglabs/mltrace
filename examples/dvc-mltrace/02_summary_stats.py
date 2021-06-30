3a0a1ea540ce92f09d94b7008d20ef6b3c8c3ee9
jeanne <jiawenchoo@gmail.com>
2021-06-30 22:19:30 +0800

    fixed linting errors (again)

--- examples/dvc-mltrace/02_summary_stats.py
+++ examples/dvc-mltrace/02_summary_stats.py
@@ -1,5 +1,6 @@
-import pandas as pd 
+import pandas as pd
 from mltrace import create_component, register
+
 
 @register(component_name="summary", input_vars=["input_filepath"])
 def summary(input_filepath: str) -> str:
@@ -14,10 +15,11 @@ def summary(input_filepath: str) -> str:
     print('counting number of columns')
     print(a.shape[1])
 
-if __name__=="__main__":
-   create_component(
+
+if __name__ == "__main__":
+    create_component(
       name="summary",
       description="print some simple stats about dataset",
       owner="jeanne",
       tags=['eda'],)
-   summary('data/abalone.csv')
+    summary('data/abalone.csv')
