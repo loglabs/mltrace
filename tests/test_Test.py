7c461217d81d2e6cdc097a1dcc43457d9c8672de
Aditi Mahajan <aditi.mahajan@berkeley.edu>
2021-10-28 18:04:52 -0700

    Test testing error

--- tests/test_Test.py
+++ tests/test_Test.py
@@ -36,4 +36,32 @@ class TestTest(unittest.TestCase):
         self.assertTrue(val[0]==120)
 
     def testRunsOnlyTestFunc(self):
-        return
+        class DummyTest(base_test.Test):
+            def __init__(self):
+                super().__init__("Dummy")
+
+            def testCorrect(self, n: int):
+                print("DEBUG: testCorrect called")
+
+            def TestNotCorrect(self, n: int):
+                raise Exception("TestNotCorrect called!")
+
+            def notCorrect(self, n: int):
+                raise Exception("notCorrect called!")
+
+            def Test(self, n: int):
+                raise Exception("Test called!")
+
+        c = base_component.Component("aditi", "test", "test_description", afterTests=[DummyTest])
+
+        @c.run(
+            component_name="test_component",
+            input_vars=[],
+            output_vars=["n"],
+        )
+        def function():
+            n = 100
+            return
+
+        function()
+
