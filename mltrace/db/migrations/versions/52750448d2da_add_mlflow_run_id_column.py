8784d0bb10d002a7d64ab1fdafa5a8eed2baa82b
Shreya Shankar <ss.shankar505@gmail.com>
2022-01-28 16:49:50 -0800

    Redoing alembic version

--- mltrace/db/migrations/versions/52750448d2da_add_mlflow_run_id_column.py
+++ mltrace/db/migrations/versions/52750448d2da_add_mlflow_run_id_column.py
@@ -1,7 +1,7 @@
 """add_mlflow_run_id_column
 
 Revision ID: 52750448d2da
-Revises: 331edc89f24b
+Revises: c41d714da766
 Create Date: 2021-12-01 14:17:19.791799
 
 """
@@ -10,17 +10,25 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '52750448d2da'
-down_revision = 'c41d714da766'
+revision = "52750448d2da"
+down_revision = None
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.add_column("component_runs",
-                  sa.Column("mlflow_run_id",
-                            sa.String, default=None))
+    op.add_column(
+        "component_runs", sa.Column("mlflow_run_id", sa.String, default=None)
+    )
+    op.add_column(
+        "component_runs", sa.Column("mlflow_run_params", sa.PickleType)
+    )
+    op.add_column(
+        "component_runs", sa.Column("mlflow_run_metrics", sa.PickleType)
+    )
 
 
 def downgrade():
     op.drop_column("component_runs", "mlflow_run_id")
+    op.drop_column("component_runs", "mlflow_run_params")
+    op.drop_column("component_runs", "mlflow_run_metrics")
