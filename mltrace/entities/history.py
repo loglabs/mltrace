e1c76e36e0d1126549d18cd66b990acc439be880
Shreya Shankar <ss.shankar505@gmail.com>
2022-03-30 10:49:43 -0700

    Moving store object out of history attributes

--- mltrace/entities/history.py
+++ mltrace/entities/history.py
@@ -1,5 +1,3 @@
-
-
 import copy
 from datetime import datetime
 
@@ -13,15 +11,16 @@ class History(object):
         componentName: str,
     ):
         self.component_name = componentName
-        self.store = Store(clientUtils.get_db_uri())
 
     def get_runs_by_time(
         self,
         start_time: datetime = datetime.min,
         end_time: datetime = datetime.max,
     ):
-        history_runs = self.store.get_history(
-            self.component_name, None, start_time, end_time)
+        store = Store(clientUtils.get_db_uri())
+        history_runs = store.get_history(
+            self.component_name, None, start_time, end_time
+        )
         history_runs = clientUtils.convertToClient(history_runs)
         return history_runs
 
@@ -30,16 +29,24 @@ class History(object):
         front_idx: int,
         last_idx: int,
     ):
-        history_runs = self.store.get_component_runs_by_index(
-            self.component_name, front_idx, last_idx)
+        store = Store(clientUtils.get_db_uri())
+        history_runs = store.get_component_runs_by_index(
+            self.component_name, front_idx, last_idx
+        )
         history_runs = clientUtils.convertToClient(history_runs)
         return history_runs
 
     def __getitem__(self, index):
-        history_run = self.store.get_component_runs_by_index(
-            self.component_name, index, index + 1)
+        store = Store(clientUtils.get_db_uri())
+        history_run = store.get_component_runs_by_index(
+            self.component_name, index, index + 1
+        )
         history_run = clientUtils.convertToClient(history_run)
         return history_run
 
     def __len__(self):
-        return self.store.get_component_runs_count(self.component_name)
+        store = Store(clientUtils.get_db_uri())
+        return store.get_component_runs_count(self.component_name)
+
+    def __repr__(self) -> str:
+        return f"History({self.component_name})"
