61226334328d61616da30061dec9de1eb90b9462
Boyuan Deng <dengboyuan2001@berkeley.edu>
2022-04-18 22:35:06 -0700

    tests for cr history indexing finished

--- tests/test_cr_history_indexing.py
+++ tests/test_cr_history_indexing.py
@@ -28,94 +28,100 @@ def isEqualComponentRun(crOne, crTwo):
 
 class TestComponentRunHistory(unittest.TestCase):
     def setUp(self):
-        set_db_uri("test")
-        self.new_component = TestHistoryComponent("history_test_six", "boyuan")
-
-        @self.new_component.run(auto_log=True)
-        def function(num):
-            num += 2
-            return num ** 2
+        self.store = Store("test")
 
         # initialize four component run for TestHistoryComponent
-        for i in range (0, 4):
-            function(i)
+        self.store.create_component("mock_component", "", "")
+        num_runs = 4
+        for i in range(1, num_runs + 1):
+            inp = self.store.get_io_pointer("inp")
+            out = self.store.get_io_pointer("out")
+            cr = self.store.initialize_empty_component_run("mock_component")
+            cr.set_start_timestamp()
+            cr.set_end_timestamp()
+            cr.add_input(inp)
+            cr.add_output(out)
+            self.store.set_dependencies_from_inputs(cr)
+            self.store.commit_component_run(cr)
 
-        self.historyLength = len(self.new_component.history)
-        self.firstComponentRun = self.new_component.history.get_runs_by_index(0, 1)
-        self.lastComponentRun = self.new_component.history.get_runs_by_index(self.historyLength - 1, self.historyLength)[0]
-        self.secondAndThirdComponentRun = self.new_component.history.get_runs_by_index(1, 3)
+        self.historyLength = self.store.get_component_runs_count("mock_component")
+        self.firstComponentRun = self.store.get_component_runs_by_index("mock_component", 0, 1)[0]
+        self.lastComponentRun = self.store.get_component_runs_by_index("mock_component", self.historyLength - 1, self.historyLength)[0]
+        self.secondAndThirdComponentRun = self.store.get_component_runs_by_index("mock_component", 1, 3)
 
     def testPstPstIndex(self):
 
         # case 1: (0, 0) return zero componentRun
-        resCrList = self.new_component.history.get_runs_by_index(0, 0)
+        resCrList = self.store.get_component_runs_by_index("mock_component", 0, 0)
         self.assertEqual(len(resCrList), 0)
 
         # case 2: (0, 1) return the first componentRun
-        resCrList = self.new_component.history.get_runs_by_index(0, 1)
+        resCrList = self.store.get_component_runs_by_index("mock_component", 0, 1)
         self.assertEqual(len(resCrList), 1)
         self.assertTrue(isEqualComponentRun(resCrList[0], self.firstComponentRun))
 
         # case 3: (len(componentRun) - 1, len(componentRun)) return the last componentRun
-        resCrList = self.new_component.history.get_runs_by_index(self.historyLength - 1, self.historyLength)
+        resCrList = self.store.get_component_runs_by_index("mock_component", self.historyLength - 1, self.historyLength)
         self.assertEqual(len(resCrList), 1)
-        self.assertEqual(isEqualComponentRun(resCrList[0], self.lastComponentRun))
+        self.assertTrue(isEqualComponentRun(resCrList[0], self.lastComponentRun))
 
         # case 4: (1, 3) return the second and third componentRun given len(componentRun) >=3)
-        resCrList = self.new_component.history.get_runs_by_index(1, 3)
+        resCrList = self.store.get_component_runs_by_index("mock_component", 1, 3)
         self.assertEqual(len(resCrList), 2)
         for idx, cr in enumerate(resCrList):
             if idx >= len(self.secondAndThirdComponentRun):
                 break
             else:
-                self.assertEqual(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
+                self.assertTrue(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
 
 
     def testPstNgtIndex(self):
 
         # case 1: (0, -len(componentRun) + 1) return first componentRun)
-        resCrList = self.new_component.history.get_runs_by_index(0, -self.historyLength + 1)
+        resCrList = self.store.get_component_runs_by_index("mock_component", 0, -self.historyLength + 1)
         self.assertEqual(len(resCrList), 1)
-        self.assertEqual(isEqualComponentRun(resCrList[0], self.firstComponentRun))
+        self.assertTrue(isEqualComponentRun(resCrList[0], self.firstComponentRun))
 
         # case 2: cannot retrieve last componentRun this way
 
         # case 3: (1, -len(componentRun) + 3) return second and third componentRun given len(componentRun) >=4)
-        resCrList = self.new_component.history.get_runs_by_index(1, -self.historyLength + 3)
+        resCrList = self.store.get_component_runs_by_index("mock_component", 1, -self.historyLength + 3)
         self.assertEqual(len(resCrList), 2)
         for idx, cr in enumerate(resCrList):
-             self.assertEqual(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
+             self.assertTrue(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
+
 
     def testNgtPstIndex(self):
 
         # case 1: (-len(componentRun), 1) return the first componentRun)
-        resCrList = self.new_component.history.get_runs_by_index(-self.historyLength, 1)
+        resCrList = self.store.get_component_runs_by_index("mock_component", -self.historyLength, 1)
         self.assertEqual(len(resCrList), 1)
-        self.assertEqual(isEqualComponentRun(resCrList[0], self.firstComponentRun))
+        self.assertTrue(isEqualComponentRun(resCrList[0], self.firstComponentRun))
 
         # case 2: (-1, len(componentRun)) return the last componentRun)
-        resCrList = self.new_component.history.get_runs_by_index(-1, self.historyLength)
-        self.assertEqual(len(resCrList),)
-        self.assertEqual(isEqualComponentRun(resCrList[0], self.lastComponentRun))
+        resCrList = self.store.get_component_runs_by_index("mock_component", -1, self.historyLength)
+        self.assertEqual(len(resCrList), 1)
+        self.assertTrue(isEqualComponentRun(resCrList[0], self.lastComponentRun))
 
         # case 3: (-len(componentRun) + 1, 3) return second and third componentRun given len(componentRun) >=3)
-        resCrList = self.new_component.history.get_runs_by_index(-self.historyLength + 1, 3)
+        resCrList = self.store.get_component_runs_by_index("mock_component", -self.historyLength + 1, 3)
         self.assertEqual(len(resCrList), 2)
         for idx, cr in enumerate(resCrList):
-             self.assertEqual(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
+             self.assertTrue(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
 
 
     def testNgtNgtIndex(self):
 
         # case 1: (-len(componentRun), -len(componentRun) + 1) return first componentRun)
-        resCrList = self.new_component.history.get_runs_by_index(-self.historyLength, -self.historyLength + 1)
+        resCrList = self.store.get_component_runs_by_index("mock_component", -self.historyLength, -self.historyLength + 1)
         self.assertEqual(len(resCrList), 1)
-        self.assertEqual(isEqualComponentRun(resCrList[0], self.firstComponentRun))
+        self.assertTrue(isEqualComponentRun(resCrList[0], self.firstComponentRun))
 
         # case 2: cannot retrieve last componentRun this way
 
         # case 3: (-len(componentRun) + 1, -len(componentRun) + 3) return second and third componentRun given len(componentRun) >=4)
-        resCrList = self.new_component.history.get_runs_by_index(-self.historyLength + 1, -self.historyLength + 3)
+        resCrList = self.store.get_component_runs_by_index("mock_component", -self.historyLength + 1, -self.historyLength + 3)
         self.assertEqual(len(resCrList), 2)
         for idx, cr in enumerate(resCrList):
-             self.assertEqual(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
+             self.assertTrue(isEqualComponentRun(cr, self.secondAndThirdComponentRun[idx]))
+
